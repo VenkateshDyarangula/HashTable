4ecace738436caf0d3ad9cb03a7d38ae3590b5d5
venkatesh179 <venkateshdyarangula@gmail.com>
2021-04-18 18:40:00 +0530

    UC2 Added

--- MyMapNode.cs
+++ MyMapNode.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace HashTableUC1
+namespace UC2_WordFreq_InPara
 {
     class MyMapNode<K, V>
     {
@@ -12,8 +12,8 @@ namespace HashTableUC1
         {
             this.size = size;
             this.items = new LinkedList<KeyValue<K, V>>[size];
-        }
 
+        }
         public void Add(K key, V value)
         {
             int position = GetArrayPosition(key);  // |-5| =5 |3|=3 |-3|=3
@@ -21,7 +21,39 @@ namespace HashTableUC1
             KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
             linkedList.AddLast(item);
         }
-
+        public void frequencyOfWords(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    foundItem = item;
+                    string str = foundItem.Value.ToString();
+                    Console.WriteLine("found data = " + str);
+                    string[] arr = str.Split(" ");
+                    Dictionary<string, int> dict = new Dictionary<string, int>();
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (dict.ContainsKey(arr[i]))
+                        {
+                            dict[arr[i]] = dict[arr[i]] + 1;
+                        }
+                        else
+                        {
+                            dict.Add(arr[i], 1);
+                        }
+                    }
+                    foreach (KeyValuePair<String, int> entry in dict)
+                    {
+                        Console.WriteLine(entry.Key + " - " +
+                                          entry.Value);
+                    }
+                }
+            }
+        }
         public void Remove(K key)
         {
             int position = GetArrayPosition(key);
@@ -58,7 +90,10 @@ namespace HashTableUC1
 
         protected int GetArrayPosition(K key)
         {
+            //Console.WriteLine("position given by gethashcode method " + key.GetHashCode());
+
             int position = key.GetHashCode() % size;
+            //  Console.WriteLine("position given by gethashcode method "+position);
             return Math.Abs(position);
         }
 
@@ -74,10 +109,10 @@ namespace HashTableUC1
         }
     }
 
-    public struct KeyValue<k, v>
+    public class KeyValue<k, v>
     {
         public k Key { get; set; }
         public v Value { get; set; }
-    }
 
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,27 +1,18 @@
 ﻿using System;
 
-namespace HashTableUC1
+namespace UC2_WordFreq_InPara
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hash table demo"); //() []
+            Console.WriteLine("Hash table demo"); 
             MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            hash.Add("0", "To");
-            hash.Add("1", "be");
-            hash.Add("2", "or");
-            hash.Add("3", "not");
-            hash.Add("4", "to");
-            hash.Add("5", "be");
-            string hash5 = hash.Get("5");
-            Console.WriteLine("5th index value: " + hash5);
-            string hash2 = hash.Get("2");
-            Console.WriteLine("2th index value: " + hash2);
-            Console.ReadKey();
+            hash.Add("0", "To be or not to be");
+            hash.frequencyOfWords("0");
+            hash.Add("1", "Paranoids are not Paranoids because they are Paranoids but because they keep putting themselves deliberatly into paranoids avoidable situations");
+            hash.frequencyOfWords("1");
+
         }
-
-
-
     }
 }
